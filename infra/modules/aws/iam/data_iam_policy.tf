633a58d021d617a0467f374e2398aaa27cd6c204
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-09-10 23:06:32 +0300

    :recycle: feat: updated infra code

--- infra/modules/aws/iam/data_iam_policy.tf
+++ infra/modules/aws/iam/data_iam_policy.tf
@@ -1,12 +1,11 @@
+locals {
+  default_effect= "Allow"
+}
+
 data "aws_iam_policy_document" "dummy_iam_policy" {
   statement {
     actions   = var.actions
     resources = var.resources
-    effect    = "Allow"
+    effect    = local.default_effect
   }
-  # statement {
-  #   actions   = ["s3:*"]
-  #   resources = [aws_s3_bucket.bucket.arn]
-  #   effect = "Allow"
-  # }
 }
